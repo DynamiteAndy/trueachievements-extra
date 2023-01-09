1e5c10e9e3fd8aae29336f0e26cced7950b1ced2
andrewcartwright1 <andrew.cartwright@bedegaming.com>
2023-01-09 21:29:03 +0000

    Replace terser minify html with minify-html

--- build/webpack.base.conf.ts
+++ build/webpack.base.conf.ts
@@ -2,10 +2,24 @@ import { resolve } from 'path';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import StatoscopeWebpackPlugin from '@statoscope/webpack-plugin';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import { minify } from '@minify-html/node';
 import Handlebars from 'handlebars';
 import register from './handlebars/register';
 
 register();
+
+const minifyHtmlConfig = {
+  do_not_minify_doctype: false,
+  ensure_spec_compliant_unquoted_attribute_values: false,
+  keep_closing_tags: true,
+  keep_html_and_head_opening_tags: false,
+  keep_spaces_between_attributes: false,
+  keep_comments: false,
+  minify_css: true,
+  minify_js: true,
+  remove_bangs: true,
+  remove_processing_instructions: true
+};
 
 export const baseConfig = {
   resolve: {
@@ -27,19 +41,17 @@ export const baseConfig = {
         test: /\.hbs$/i,
         loader: 'html-loader',
         options: {
-          minimize: process.env.webpack_minimize ? true : false,
-          preprocessor: (content, loaderContext) => {
-            let result: string;
-
+          preprocessor: (content: string, loaderContext) => {
             try {
-              result = Handlebars.compile(content)({});
+              let result = Handlebars.compile(content)({});
+              result = minify(Buffer.from(result), minifyHtmlConfig).toString();
+
+              return result;
             } catch (error) {
               loaderContext.emitError(error);
 
               return content;
             }
-
-            return result;
           }
         }
       },
@@ -47,7 +59,12 @@ export const baseConfig = {
         test: /\.html$/i,
         loader: 'html-loader',
         options: {
-          minimize: process.env.webpack_minimize ? true : false
+          minimize: process.env.webpack_minimize ? true : false,
+          preprocessor: (content: string) => {
+            const result = minify(Buffer.from(content), minifyHtmlConfig).toString();
+
+            return result;
+          }
         }
       },
       {
